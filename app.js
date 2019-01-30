de5bef75be6387df2896a6a5f54a34275fb2a01f
Harman Bhatti <hps@jampas.net>
2019-01-30 23:39:36 +0530

    checking if more repos are available and activating next button

--- app.js
+++ app.js
@@ -4,32 +4,13 @@ var getRepos = () => {
   username = document.getElementById('username').value;
   const url = `https://api.github.com/users/${username}/repos`;
   console.log(url);
-  fetch(url)
-    .then(result => result.json())
-    .then((data) => {
-      repoList = data;
-      let el = document.getElementById('repoCount');
-      init(el);
-      let list = document.getElementById("repoList");
-      init(list);
-      el.innerText = data.length;
-      for (let i = 0; i < data.length; i++) {
-        let repo = data[i].name;
-        let btn = document.createElement("BUTTON");
-        btn.setAttribute("class", "btn btn-info btn-sm");
-        btn.setAttribute("onClick", `getCommits(${i})`);
-        let content = document.createTextNode(repo);
-        btn.appendChild(content);
-        list.appendChild(btn);
-      }
-    })
-    .catch(error => console.log(error))
+  loadRepositories(url);
 }
 
 var getCommits = (number) => {
   const repoName = repoList[number].name;
   const urlRepo = `https://api.github.com/repos/${username}/${repoName}/commits`;
-  console.log(number);
+  console.log(urlRepo);
   fetch(urlRepo)
     .then(result => result.json())
     .then((data) => {
@@ -84,6 +65,54 @@ var getCommitDetails = (repo, sha) => {
     })
 }
 
+var loadRepositories = (url) => {
+  fetch(url)
+    .then(result => {
+      console.log(result.headers.get('Link'));
+      if (result.headers.get('Link')) {
+        let linkHeader = result.headers.get('Link');
+        if (linkHeader.includes('next')) {
+          console.log('there is a next page');
+          let url = getPageUrl(linkHeader, 'next');
+          let btn = document.getElementById('next-repoList');
+          btn.disabled = false;
+          btn.setAttribute("onClick", `loadData('${url}')`);
+        }
+      }
+      return result.json();
+    })
+    .then((data) => {
+
+      repoList = data;
+      let el = document.getElementById('repoCount');
+      init(el);
+      let list = document.getElementById("repoList");
+      init(list);
+      el.innerText = data.length;
+      for (let i = 0; i < data.length; i++) {
+        let repo = data[i].name;
+        let btn = document.createElement("BUTTON");
+        btn.setAttribute("class", "btn btn-info btn-sm");
+        btn.setAttribute("onClick", `getCommits(${i})`);
+        let content = document.createTextNode(repo);
+        btn.appendChild(content);
+        list.appendChild(btn);
+      }
+    })
+    .catch(error => console.log(error))
+}
+
 var init = (el) => {
   el.innerText = '';
+}
+
+var getPageUrl = (header, nav) => {
+  let links = header.split(', ');
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].includes(nav)) {
+      end = links[i].indexOf('>');
+      var url = links[i].substring(1,end);
+    }
+  }
+  return url;
 }
