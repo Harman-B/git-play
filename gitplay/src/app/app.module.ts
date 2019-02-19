ed255927892a0fb2a350366007700fce4b512231
Harman Bhatti <hps@jampas.net>
2019-02-19 19:23:35 +0530

    removed component repositories

--- gitplay/src/app/app.module.ts
+++ gitplay/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LinkUserComponent } from './link-user/link-user.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RepositoriesComponent } from './repositories/repositories.component';
 import { UserService } from './user.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     LinkUserComponent,
-    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
