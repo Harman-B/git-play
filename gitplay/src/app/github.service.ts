e99a473a5041f570a839e017d3b3a82ddd484220
Harman Bhatti <hps@jampas.net>
2019-02-21 01:10:35 +0530

    #14 authorisation done by adding access token to url

--- gitplay/src/app/github.service.ts
+++ gitplay/src/app/github.service.ts
@@ -10,6 +10,8 @@ export class APIService {
 
     githubAPI(url:string) {
         console.log(url);
+        
+        url = url + '?access_token=03abfa863fe6904145a8bb97df98e302290bbb8f';
 
         const response = this.http.get(url, {
             headers: new HttpHeaders({'Content-Type': 'application/json', 'Authorization': '03abfa863fe6904145a8bb97df98e302290bbb8f'}),
